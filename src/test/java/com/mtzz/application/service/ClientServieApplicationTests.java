e891ed3171b898d3830374130bf6337506579800
Mateus Henrique <mateushcandido5@gmail.com>
2023-02-26 16:31:31 -0300

    Início de projeto

---
+++ src/test/java/com/mtzz/application/service/ClientServieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtzz.application.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientServieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
