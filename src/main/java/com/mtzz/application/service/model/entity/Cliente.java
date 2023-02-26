d92d7fa78f4dbcfecb3b20a550d71a79e7082a0f
Mateus Henrique <mateushcandido5@gmail.com>
2023-02-26 16:32:04 -0300

    Criação da entidade cliente

---
+++ src/main/java/com/mtzz/application/service/model/entity/Cliente.java
@@ -0,0 +1,27 @@
+package com.mtzz.application.service.model.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cliente
+{
+    @Id @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 150)
+    private String nome;
+
+    @Column(nullable = false, length = 11)
+    private String cpf;
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate dataCadastro;
+}
