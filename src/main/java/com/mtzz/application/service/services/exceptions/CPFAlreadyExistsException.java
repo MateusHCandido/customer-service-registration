3ed654b162b188095e51bb8d39100807ee3736a0
Mateus Henrique <mateushcandido5@gmail.com>
2023-02-26 20:04:46 -0300

    Exceções para os métodos addCliente e checkIfCpfAlreadyExist

---
+++ src/main/java/com/mtzz/application/service/services/exceptions/CPFAlreadyExistsException.java
@@ -0,0 +1,9 @@
+package com.mtzz.application.service.services.exceptions;
+
+public class CPFAlreadyExistsException extends RuntimeException
+{
+    public CPFAlreadyExistsException(String message)
+    {
+        super(message);
+
+    }}
---
+++ src/main/java/com/mtzz/application/service/services/exceptions/CreateClientException.java
@@ -0,0 +1,9 @@
+package com.mtzz.application.service.services.exceptions;
+
+public class CreateClientException extends RuntimeException
+{
+    public CreateClientException(String message)
+    {
+        super(message);
+    }
+}
